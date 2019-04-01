cc226094d15c1c5b7ef20b17d81de0d543ebd308
Jim DeSelms <jdeselms@vistaprint.com>
2019-04-01 17:31:54 -0400

    Add a little more documentation

--- src/Bifoql.Playpen/ClientApp/src/components/Integrating.js
+++ src/Bifoql.Playpen/ClientApp/src/components/Integrating.js
@@ -9,30 +9,41 @@ var bookJson = JSON.stringify({books: [
   { name: '1984', author: 'George Orwell'}
 ]}, null, 2);
 
+var code = `public async <object> RunQuery(string query, IReadOnlyDictionary<string, object> arguments)
+{
+    var obj = new YourRootObject();
+    var query = Bifoql.Query.Compile(query);
+    return await query.Run(obj, arguments).Result;
+}
+`;
+
 export const Integrating = () => (
   <div className='bifoql-doc'>
     <h1>Integrating with Bifoql</h1>
 
     <p>
-        Integrating with Bifoql is not difficult. There are a few steps:
+        Integrating with Bifoql is not difficult; you'll probably want to expose it as an endpoint from an MVC application. At it's core, you've got this snippet of 
+        code to execute a Bifoql query and get the result:
 
-        <ol>
-            <l1>Define your data objects</l1>
-            <l1>Defining a schema</l1>
-            <li>Create an HTTP service</li>
-            <l1>Expose an endpoint for accepting queries</l1>
-            <l1>Run the query and return the result</l1>
-        </ol>
+        <pre>{code}</pre>
     </p>
 
-    <h2>Defining your data</h2>
-
-    <p>At the highest level, your Bifoql service returns a single root object. This object typically returns a 
-        number of fields which in turn can be simple POCOs, or Bifoql lookups or indexes.</p>
-
-    <p>Each property on a Bifoql object should be a <code>System.Func&lt;object></code>.</p>
-
-    <Playpen readOnly={true} query="using foo.boogers;" />
+    <p>
+        In this example, we take in some parameters; you can get them from the query string. Then, we create an instance of our root object. This object can be any C# object that follows a few rules:
+        <ul>
+            <li>It contains a number of public properties with getters; each of these properties will be exposed to the client.</li>
+            <li>The type of these properties can be:
+                <ul>
+                    <li>Simple types like strings, ints, etc.</li>
+                    <li>Complex types that also follow these properties (you can nest these objects as deeply as you want.)</li>
+                    <li>A Bifoql type (IBifoqlLookup, IBifoqlMapSync, IBifoqlIndexedLookup, etc.</li>
+                    <li>Task&lt;T> where T is a type that follows these rules</li>
+                    <li>An IEnumerable&lt;T> of a type that follows these rules</li>
+                    <li>An IReadOnlyDictionary&lt;string, T> where T is a a type that follows these rules</li>
+                </ul>
+            </li>
+        </ul>
+    </p>
 
 </div>
 );
--- src/Bifoql.Playpen/ClientApp/src/components/VsGraphql.js
+++ src/Bifoql.Playpen/ClientApp/src/components/VsGraphql.js
@@ -14,9 +14,41 @@ export const VsGraphql = () => (
     <h1>Why not GraphQL?</h1>
 
     <p>
-      Bifoql stands for BIg Freaking Object Query Language. The idea is that you have service that exposes a "Big Freaking Object",
-      and your clients send queries to filter that big object down and massage it into a smaller object that just gives
-      you exactly what you want.
+      Bifoql has a lot of similarites to GraphQL; when I first wrote Bifoql, I didn't know GraphQL existed, and I wrote it to solve a similar problem, but I had different inspirations.
+      I was initially inspired by AWS's command line interfaces, which allows you to specify filters to your commands. These commands allow you to pick and choose the fields you want, and to remap
+      the results into a completely different shape. I was also inspired by the language "JMESPath" which solves a similar problem by remapping a JSON object into a different JSON object.
+    </p>
+
+    <p>
+      But I also wanted to be able to write generic APIs for services so that clients didn't have to bug the service providers for new endpoints for every use case. This is the basic functionality that
+      GraphQL provides, however, it doesn't have the remapping capabilities of Bifoql.
+    </p>
+
+    <p>
+      GraphQL is great. It has support for many languages, it's also actively supported by Facebook. It has a user community that can help you solve any problem. It also has type safety, and it supports subscriptions.
+      It's also a little bit simpler language.
+    </p>
+
+    <p>
+      Bifoql allows you to write more complex queries; that way you can move complexity out of your client-side code and put it in your queries. It also allows you to make much more compact results without
+      making changes to the GraphQL API. Some examples:
+      <ul>
+        <li>Using the <code>[? condition]</code> construct, you can filter results server-side</li>
+        <li>There's a <code>length()</code> built-in function so that you can return the length of an array; in GraphQL you'd have to download the whole list and count it client side. Or, 
+        the server would have to explicitly add a field to get the count.</li>
+        <li>You can remap a result to collapse it down to a single field if you want. In GraphQL, if you had a deeply-nested structure, and only wanted a single field, you'd have to download a large
+          structure that ultimately contains a single leaf node. In Bifoql, if you just want a single string, you just a single string back.
+        </li>
+      </ul>
+    </p>
+
+    <p>
+      In GraphQL, all nodes are download simultaneously, and as a result, you can't write a query to look up another record based on the result of another subquery. Bifoql also takes advantage of 
+      parallelization, but it also lets you chain together your queries so that the result of one query can be used as a parameter in another query.
+    </p>
+
+    <p>
+      Are these additional features in Bifoql compelling enough to cause people to adopt Bifoql over GraphQL? Maybe?!
     </p>
 
 </div>
